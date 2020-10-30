41622658c6daa2a92d25d629b4efd7547d24e6bc
Yunaka1269 <yuunakanishi@hotmail.com>
2020-10-30 01:00:10 -0700

    change

--- Queries/employee_database.sql
+++ Queries/employee_database.sql
@@ -55,9 +55,6 @@ CREATE TABLE titles (
   PRIMARY KEY (emp_no, title, from_date)
 );
 
-DROP TABLE dept_emp;
-DROP TABLE titles;
-
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
@@ -82,10 +79,6 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT * FROM retirement_info;
-
-DROP TABLE retirement_info;
 
 -- Create new table for retiring employees
 SELECT emp_no, first_name, last_name
@@ -143,8 +136,6 @@ LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
 
-SELECT * FROM current_emp;
-
 -- Employee count by department number
 SELECT COUNT(ce.emp_no), de.dept_no
 INTO current_emp_count
@@ -153,9 +144,6 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
-
-SELECT * FROM current_emp_count;
-DROP TABLE current_emp_count;
 
 SELECT * FROM salaries
 ORDER BY to_date DESC;
@@ -197,8 +185,6 @@ ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
 
-SELECT * FROM sales_info;
-
 SELECT ri.emp_no,
 ri.first_name,
 ri.last_name,
@@ -229,6 +215,7 @@ SELECT*FROM retiring_titles;
 SELECT*FROM retiring_title;
 SELECT*FROM unique_titles;
 SELECT*FROM retirement_titles;
+SELECT*FROM unique_titles_new;
 
 --module challenge (Deliverable 1)
 SELECT e.emp_no,
@@ -279,4 +266,154 @@ INNER JOIN titles as t
 ON (e.emp_no=t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
-ORDER BY emp_no ASC
+ORDER BY emp_no ASC;
+
+SELECT COUNT(title),title
+INTO mentorship_eligibility_title
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY count DESC;
+
+--expand eligibility for those whose b-day b/w 1962 to 1965
+SELECT DISTINCT ON (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO aaa
+FROM employees as e
+INNER JOIN dept_emp as de 
+ON (e.emp_no=de.emp_no)
+INNER JOIN titles as t 
+ON (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no ASC;
+
+SELECT COUNT(title),title
+INTO mentorship_eligibility1962_1965
+FROM aaa
+GROUP BY title
+ORDER BY count DESC;
+
+--add gender
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+e.gender,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO retirement_titles_new
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no=ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+--delete dup emp_no
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title,
+gender
+INTO unique_titles_new
+FROM retirement_titles_new
+ORDER BY emp_no ASC, from_date DESC;
+
+--count retiring title by gender 
+SELECT COUNT(gender),gender
+INTO retiring_title_new
+FROM unique_titles_new
+GROUP BY gender;
+
+--count employees by gender
+SELECT COUNT (gender), gender
+INTO gender
+FROM employees
+GROUP BY gender;
+
+SELECT * FROM full_titles;
+SELECT * FROM full_emp_gender;
+
+--add gender
+SELECT DISTINCT ON (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+e.gender,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility_gender
+FROM employees as e
+INNER JOIN dept_emp as de 
+ON (e.emp_no=de.emp_no)
+INNER JOIN titles as t 
+ON (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no ASC;
+
+SELECT COUNT(gender),gender
+-- INTO mentorship_eligibility_title
+FROM mentorship_eligibility_gender
+GROUP BY gender;
+
+SELECT DISTINCT ON (emp_no) e.emp_no, 
+e.first_name, 
+e.last_name, 
+e.gender, 
+s.salary, 
+de.from_date,
+de.to_date
+INTO full_emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no=s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no ASC, from_date DESC;
+
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO full_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no=ti.emp_no)
+WHERE (ti.to_date = '9999-01-01')
+ORDER BY emp_no ASC;
+
+SELECT fe.emp_no,
+fe.first_name,
+fe.last_name,
+fe.gender,
+ft.title,
+fe.salary,
+fe.from_date,
+fe.to_date
+INTO full_emp_gender
+FROM full_emp_info AS fe
+INNER JOIN full_titles as ft
+ON (fe.emp_no=ft.emp_no)
+ORDER BY emp_no ASC;
+
+SELECT AVG (salary), gender, title
+INTO avg_salary
+FROM full_emp_gender
+GROUP BY title, gender
+
+SELECT * FROM current_emp_count;
+
+SELECT COUNT(gender), title, gender
+-- INTO retiring_title_new
+FROM unique_titles_new
+GROUP BY gender, title
+ORDER BY title;
